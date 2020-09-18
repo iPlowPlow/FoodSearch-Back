cee732c548e838f3a251f844f5fb6adc9a6cfe0e
iPlowPlow <goasguen.loic@gmail.com>
2020-09-18 12:37:56 +0200

    init project

---
+++ foodsearch/src/main/java/fr/iplowplow/foodsearch/FoodsearchApplication.java
@@ -0,0 +1,13 @@
+package fr.iplowplow.foodsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodsearchApplication.class, args);
+    }
+
+}
---
+++ foodsearch/src/main/java/fr/iplowplow/foodsearch/controllers/MapsController.java
@@ -0,0 +1,10 @@
+package fr.iplowplow.foodsearch.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MapsController {
+
+
+
+}
